f1b7248dbe25d9023b321714cc4831ccdf4a6e3d
Mohammed Elsayed <moerty43@gmail.com>
2021-03-16 19:28:11 -0400

    Update Math.js

---
+++ src/components/Math.js
@@ -0,0 +1,10 @@
+// NAME = (I)tem,(T)ier,(L)ine
+// Weapon, Emblem, Secondary, Hat, Top/Overall, Bot, Shoe, Shoulder/Cape, Glove, Belt, Accesory , Heart
+var WL1R = {
+    'Stat12'   : .097561,
+    'ATT12'    : .048780,
+    'Crit'     : .048780,
+    'DMG'      : .048780,
+    'AllStat9' : .073171,
+    
+};
