4e64f3edd6d99b8e4fb1f2754b2c667390b632cb
Mohammed Elsayed <moerty43@gmail.com>
2021-03-16 18:59:48 -0400

    data entry start
    start

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
-import { Route, Router } from 'react-router-dom';
 import Homepage from './Homepage';
-import history from '../utils/history';
 
 function App() {
   return (
-    <Router history ={history}>
-      <Route path="/" component={Homepage} />
-    </Router>
+    <Homepage />
   );
 }
 
